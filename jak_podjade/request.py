28c79229cf38cc2f0af5ff0ffc988625fb17a207
Kuba <jakubdoman2@gmail.com>
2020-02-09 00:43:25 +0100

    request.py documentation

--- jak_podjade/request.py
+++ jak_podjade/request.py
@@ -5,10 +5,21 @@ request_for_stops = "http://www.ttss.krakow.pl/internetservice/geoserviceDispatc
 
 
 def load_all_stops():
+    """
+    Funkcja wysyła zapytanie o nazwy wszystkich przystanków i zwraca słownik z nimi.
+    :return: słownik ze wszystkimi przystankami, w którym kluczem jest nazwa, a wartością id.
+    """
     request = requests.get(request_for_stops)
     return create_dict_with_all_stops(request.text)
 
 
 def load_traffic_for_stop(user_input, stop_id):
+    """
+    Funkcja wysyła zapytanie o przyjazdy na przystanek
+    i zwraca słownik z poszukiwanymi przyjazdami.
+    :param user_input: słownik z przystankiem, numerem linii i kierunkiem.
+    :param stop_id: id przystanku potrzebne do zapytania.
+    :return: zwraca słownik zawierający poszukiwane przyjazdy na przystanek.
+    """
     request = requests.get("http://www.ttss.krakow.pl/internetservice/services/passageInfo/stopPassages/stop?stop=" + stop_id + "&mode=departure&language=pl")
     return create_dict_with_results(request.text, user_input)
