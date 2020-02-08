c0ef3d4527bacbfe11ceb887e5853fdad2c7e815
Kuba <jakubdoman2@gmail.com>
2020-02-08 21:51:40 +0100

    Json.py documentation

--- jak_podjade/Json.py
+++ jak_podjade/Json.py
@@ -3,6 +3,11 @@ from collections import defaultdict
 
 
 def create_dict_with_all_stops(response):
+    """
+    Funkcja wyciąga z responsu wszystkie przystanki i tworzy słownik.
+    :param response: wynik zapytania API o listę przystanków.
+    :return: słownik ze wszystkimi przystankami, w którym kluczem jest nazwa, a wartością id.
+    """
     all_stops_dict = defaultdict(str)
     json_format = json.loads(response)
     json_values = json_format["stops"]
@@ -13,6 +18,13 @@ def create_dict_with_all_stops(response):
 
 
 def create_dict_with_results(response, user_input):
+    """
+    Funkcja filtruje response zawierający wszystkie przyjazdy na podstawie user_input
+    i zwraca słownik z wynikami filtrowania.
+    :param response: wynik zapytania API o przyjazdy na przystanek.
+    :param user_input: słownik z przystankiem, numerem linii i kierunkiem.
+    :return: zwraca słownik zawierający słowniki z poszczególnymi wynikami filtrowania.
+    """
     results = defaultdict(dict)
     json_format = json.loads(response)
     json_values = json_format["actual"]
@@ -26,6 +38,12 @@ def create_dict_with_results(response, user_input):
 
 
 def filter_results(json_values, user_input):
+    """
+    Funkcja filtruje przyjazdy na dany przystanek po numerze linii i kierunku, i zwraca liste z wynikami filtrowania.
+    :param json_values: lista słowników z wynikami zapytania o przyjazdy tramwajów na przystanek.
+    :param user_input: słownik z przystankiem, numerem linii i kierunkiem.
+    :return: zwraca listę ze słownikami spełniającymi kryteria.
+    """
     list_with_filtered_results = []
     for value in json_values:
         if user_input["LineNumber"]:
