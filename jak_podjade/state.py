9008cd67e55d4dc9f1c46774212f00f646612030
Kuba <jakubdoman2@gmail.com>
2020-02-09 14:41:37 +0100

    state.py documentation

--- jak_podjade/state.py
+++ jak_podjade/state.py
@@ -1,6 +1,16 @@
 class State:
+    """
+    Klasa przechowująca stan aplikacji, który decyduje o zakończeniu działania lub kontynuowaniu.
+    """
     def __init__(self, should_exit):
+        """
+        Konstruktor
+        :param should_exit: wartość inicjująca pole should_exit.
+        """
         self.should_exit = should_exit
 
     def change_state(self):
+        """
+        Metoda zmienia stan aplikacji.
+        """
         self.should_exit = not self.should_exit
